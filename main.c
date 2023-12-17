8a4284a08b38342977a1398b6ab7d716378971fd
Mitchell Waghorn <m.waghorn2000@gmail.com>
2023-12-17 20:06:17 +1100

    added a more robust form of getting arguments, accounts for quotations

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "file_nav.h"
+#include <stdbool.h>
 
 #define MAX_INPUT_SIZE 100
+#define LINE_CHARACTER '$'
 
 static void run_loop(void);
-static void split_str(char *str, char **arguments, int *size);
+static void get_arguments(char *str, char **arguments, int *size);
 
 int main(void) {
 	printf("Welcome to MyShell!\n - type 'help' for a list of commands\n\n");
@@ -18,16 +19,25 @@ int main(void) {
 
 static void run_loop(void) {
 	while(1) {
-		printf("$ ");
+		printf("%c ", LINE_CHARACTER);
+
+		//git a
 		char command[MAX_INPUT_SIZE];
 		fgets(command, sizeof(command), stdin);
+		size_t len = strlen(command);
+		if (len > 0 && command[len - 1] == '\n') {
+			command[len - 1] = '\0';
+		}
+
+		if (strlen(command) == 0) {
+			continue;
+		}
 		
 		char **arguments = malloc(sizeof(char) * 100);
 		int size = 0;
-		split_str(command, arguments, &size);
+		get_arguments(command, arguments, &size);
 
 		if (strcmp(arguments[0], "pwd") == 0) {
-			pwd();
 		} else if (strcmp(arguments[0], "exit") == 0) {
 			printf("exiting myShell...\n");
 			break;
@@ -35,14 +45,31 @@ static void run_loop(void) {
 	}
 }
 
-static void split_str(char *str, char **arguments, int *size) {
-	char *token;
-	const char *delim = " \t\n";
-	token = strtok(str, delim);
+static void get_arguments(char *str, char **arguments, int *length) {
+    bool in_single_quote = false;
+    bool in_double_quote = false;
+    char buffer[100];
+    int buffer_index = 0;
 
-	while (token != NULL) {
-		arguments[*size] = token;
-		*size += 1;
-		token = strtok(NULL, delim);
-	}
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!in_single_quote && !in_double_quote && (str[i] == ' ' || str[i+1] == '\0')) {
+            if (str[i+1] == '\0' && str[i] != ' ') { // Add last character if it's not a space
+                buffer[buffer_index] = str[i];
+                buffer_index++;
+            }
+            buffer[buffer_index] = '\0'; // Null-terminate the string
+            arguments[*length] = strdup(buffer); // Allocate memory and copy the buffer
+            *length += 1;
+            memset(buffer, 0, sizeof(buffer)); // Clear the buffer
+            buffer_index = 0;
+        } else if (str[i] == '\'') {
+            in_single_quote = !in_single_quote;
+        } else if (str[i] == '\"') {
+            in_double_quote = !in_double_quote;
+        } else {
+            buffer[buffer_index] = str[i];
+            buffer_index++;
+        }
+    }
 }
+
